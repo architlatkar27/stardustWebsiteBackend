ee9b6f5a4b0660d497499ea467f4c4277c7f055a
saitanmayee01 <sai.penupati@gmail.com>
2021-06-17 19:31:13 +0530

    deployment

--- StardustWebsite/settings.py
+++ StardustWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
