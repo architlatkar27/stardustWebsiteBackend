dc62fefa2887344170a35ab61212e3f32963423c
saitanmayee01 <sai.penupati@gmail.com>
2021-06-17 16:01:14 +0530

    research page

--- Research/models.py
+++ Research/models.py
@@ -11,5 +11,5 @@ class ResearchPaper(models.Model):
     year           = models.IntegerField(null=False, blank=False)
     author         = models.ForeignKey(Student, on_delete=models.PROTECT, related_name="author")
     conference     = models.CharField(max_length=200, null=False, blank=False)
-    abstract       = models.TextField(max_length=1000, null=False, blank=False)
-    pdf_link       = models.URLField(null=False, blank=False,default='None')  
+    #abstract       = models.TextField(max_length=1000, null=False, blank=False)
+    abstract_pdf      = models.URLField(null=False, blank=False)  
