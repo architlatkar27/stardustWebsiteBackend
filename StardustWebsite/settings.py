b1ac68fc9aad89a4cba0429861d26514103d8aaf
saitanmayee01 <sai.penupati@gmail.com>
2021-08-01 16:49:35 +0530

    degug=false, since in production

--- StardustWebsite/settings.py
+++ StardustWebsite/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '+6llouc2@s!&bp*%2+bqa75$+m1i$nqr!#k$f92^*m*r9el+iq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://teamstardust.herokuapp.com']
 
